4402a22dde334bb16f7349588d2c273f16ab660d
Sheldon <srdo536@aucklanduni.ac.nz>
2021-04-25 21:25:34 +1200

    fixed issues with the static reference

--- client/src/websocket/websocket-client.js
+++ client/src/websocket/websocket-client.js
@@ -7,12 +7,16 @@ export class WebSocketClient {
     static getInstance(roomId, nickname, addToQueue){
         if(!WebSocketClient.instance){
             WebSocketClient.instance = new WebSocketClient(roomId, nickname, addToQueue);
-        } 
+        } else if (WebSocketClient.instance.roomId != roomId){
+            WebSocketClient.instance.socketRef.close();
+            WebSocketClient.instance = new WebSocketClient(roomId, nickname, addToQueue);
+        }
         return WebSocketClient.instance; 
     }
 
     constructor(roomId, nickname, addToQueue){
         var self = this;
+        var roomId = roomId;
         this.addToQueue = addToQueue; 
         this.socketRef = new W3CWebSocket(`ws://localhost:8080/join?roomId=${roomId}&nickname=${nickname}`);
         
