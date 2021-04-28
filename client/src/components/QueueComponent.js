503d345dddcd6442680bd21e100f945c919139af
Sheldon <srdo536@aucklanduni.ac.nz>
2021-04-28 22:27:53 +1200

    fixed the queue

--- client/src/components/QueueComponent.js
+++ client/src/components/QueueComponent.js
@@ -37,7 +37,7 @@ export class QueueComponent extends React.Component {
         if(this.state.queue.length == 0){ return <p>No songs are in the queue</p> }
         
         return (
-            [this.state.queue].map((item, index) => <p key={index}>{item}</p>)  
+            this.state.queue.map((item, index) => <div><p key={index}>{item}</p></div>)  
         )
     }
    
