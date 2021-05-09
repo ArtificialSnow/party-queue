8ece4f721d811ad741166417fb5367c1300fb755
ArtificialSnow <plin213@aucklanduni.ac.nz>
2021-05-09 18:32:54 +1200

    Update RoomQrCode.js

--- client/src/components/RoomQrCode.js
+++ client/src/components/RoomQrCode.js
@@ -2,7 +2,7 @@ var React = require('react');
 var QRCode = require('qrcode.react');
 
 export function RoomQrCode(props) {
-    const fullUrl = `http://www.musicQ.com${props.url}`;
+    const fullUrl = `http://www.musicqueue.net/${props.url}`;
 
     return (
         <div>
